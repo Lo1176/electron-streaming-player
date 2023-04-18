3f31dca37b438fd7865e73fdd47d842d9280d2e7
Laurent Binder <laurentbinder@gmail.com>
2023-04-18 15:34:40 +0200

    add displayAlbumSongsNames and play songs

--- musehit_app/src/renderer/renderer.ts
+++ musehit_app/src/renderer/renderer.ts
@@ -2,31 +2,25 @@
 const ALBUMS = versions.findAlbumsByArtist();
 
 /** function to display album' songs */
-const displayAlbumSongsNames = () => {
-  // display songs names
-  const containerSongsList = document.getElementById("songs-list");
-  // create ul element and set its attributes.
-  const ulsongs = document.createElement("ul");
-//   ulsongs.setAttribute("style", "padding: 4px; margin: 0;");
-  ulsongs.setAttribute("id", "theSongList");
+const displayAlbumSongsNames = (album_id) => {
+  const containerSongsList = document.getElementById("songs-list"); // display songs names
+  containerSongsList.innerHTML = ""; // reset div before feeding it
+  const ulSongs = document.createElement("ul"); // create ul element and set its attributes.
+  ulSongs.setAttribute("id", "theSongList"); //   ulSongs.setAttribute("style", "padding: 4px; margin: 0;");
 
-  for (let i = 0; i < ALBUMS.length; i++) {
-    // console.log("album.length", versions.findAllSongsByAlbumID(ALBUMS[i].id).length)
-    const li = document.createElement("li"); // create li element.
-    for (
-      let j = 0;
-      j < versions.findAllSongsByAlbumID(ALBUMS[i].id).length;
-      j++
-    ) {
-      // li.innerHTML = `<a href='album_${ALBUMS[i].id}'>${ALBUMS[i].name}</a>`;	// assigning text to li using array value.
-      li.innerHTML +=
-        versions.findAllSongsByAlbumID(ALBUMS[i].id)[j].name + "<br>";
-      li.setAttribute("style", "display: block;"); // remove the bullets.
-    }
-
-    ulsongs.appendChild(li); // append li to ul.
+  const li = document.createElement("li"); // create li element.
+  const songs = versions.findAllSongsByAlbumID(album_id);
+  for (let j = 0; j < songs.length; j++) {
+    li.innerHTML +=
+      `<a 
+        id="songs_${j}" 
+        href="./../public/uploads/${songs[j].path}">"${songs[j].name} ▶️ "</a>` + "<br>";
+    li.setAttribute("style", "display: block;"); // remove the bullets.
   }
-  containerSongsList.appendChild(ulsongs); // add ul to the container.
+
+  ulSongs.appendChild(li); // append li to ul.
+
+  containerSongsList.appendChild(ulSongs); // add ul to the container.
 };
 
 /** display ALBUMS name on Homepage */
@@ -39,16 +33,11 @@ ulAlbums.setAttribute('id', 'theAlbumList');
 for ( let i = 0; i < ALBUMS.length; i++) {
     const li = document.createElement('li');	// create li element.
 
-    // li.innerHTML = `<a href='#album'>${ALBUMS[i].name}</a>`;	// assigning text to li using array value.
     li.innerHTML = `<a id="myLink" title="Click to do something"
-    href="#album" onclick="${displayAlbumSongsNames()};return false;">${ALBUMS[i].name}</a>`;	// assigning text to li using array value.
-    // li.innerHTML = versions.findAllSongsByAlbumID(ALBUMS[i].id);
+    href="#album">${ALBUMS[i].name}</a>`;	// assigning text to li using array value.
     li.setAttribute ('style', 'display: block;');	// remove the bullets.
+    li.addEventListener("click", () => { displayAlbumSongsNames(ALBUMS[i].id);});
 
     ulAlbums.appendChild(li);		// append li to ulAlbums.
 }
 albumContainer.appendChild(ulAlbums);		// add ul to the albumContainer.
-
-
-
-
