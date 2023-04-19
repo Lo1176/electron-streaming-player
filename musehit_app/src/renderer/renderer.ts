21f45b12f1930cdaa1ae58409e2a143f1e1680fa
Laurent Binder <laurentbinder@gmail.com>
2023-04-19 16:52:07 +0200

    add next song function WIP

--- musehit_app/src/renderer/renderer.ts
+++ musehit_app/src/renderer/renderer.ts
@@ -1,50 +1,76 @@
-// find all ALBUMS
-const ALBUMS = versions.findAlbumsByArtist();
+// find all albums
+const albums = versions.findAlbumsByArtist();
+console.log("🚀 ~ file: renderer.ts:3 ~ albums:", albums)
 const containerSongsList = document.getElementById("songs-list"); // display songs names
 const audioPlayer = document.getElementById("audioPlayer");
 
 /** function to pay a song */
-const playThisSong = (songPath) => {
-  audioPlayer.src = `./../public/uploads/${songPath}`;
-}
-/** function to display album' songs */
-const displayAlbumSongsNames = (album_id) => {
-  containerSongsList.innerHTML = ""; // reset div before feeding it
-  const ulSongs = document.createElement("ul"); // create ul element and set its attributes.
-  ulSongs.setAttribute("id", "theSongList"); //   ulSongs.setAttribute("style", "padding: 4px; margin: 0;");
+const playThisSong = (song, totalTracks) => {
+  console.log("🚀 ~ file: renderer.ts:8 ~ playThisSong ~ song:", song);
+  audioPlayer.src = `./../public/uploads/${song.path}`;
+  // console.log("id?: ",song.id)
 
-  const divSong = document.createElement("div"); // create div element.
-  const songs = versions.findAllSongsByAlbumID(album_id);
-  // console.log(songs) // songs = array d'object [{}, {}, ..., {}]
-  // list all songs
-  for (let i = 0; i < songs.length; i++) {
-    const figcaption = document.createElement("figcaption"); // create li element.
-    figcaption.innerText += `${songs[i].name} `;
-    figcaption.addEventListener("click", () => playThisSong(songs[i].path));
-    ulSongs.appendChild(figcaption); // append li to ul.
-  }
-
-  containerSongsList.appendChild(ulSongs); // add ul to the container.
+  audioPlayer.addEventListener("ended", () => {
+    console.log(`this song ${song.name} is finished`);
+    nextSong(song, totalTracks);
+  });
 };
 
-/** display ALBUMS name on Homepage */
+// Next song
+function nextSong({id, totalTracks}) {
+  console.log("🚀 ~ file: renderer.ts:32 ~ nextSong_id:", id)
+  id++;
+
+  if (id > totalTracks - 1) {
+    id = 0;
+  }
+
+  // loadSong(songs[songIndex]);
+
+  playThisSong(id, totalTracks);
+}
+/** function to display songs of an album */
+const displayAlbumSongsNames = ({id}) => {
+  // console.log(id)
+  containerSongsList.innerHTML = ""; // reset div before feeding it
+  const divAllSongs = document.createElement("div"); // create ul element and set its attributes.
+  divAllSongs.setAttribute("id", "theSongList"); //   divAllSongs.setAttribute("style", "padding: 4px; margin: 0;");
+
+  const songs = versions.findAllSongsByAlbumID(id);
+  const totalTracks = songs.length;
+  // console.log("🚀 ~ file: renderer.ts:52 ~ displayAlbumSongsNames ~ totalTracks:", totalTracks)
+  // list all songs
+  for (let i = 0; i < songs.length; i++) {
+    const divSong = document.createElement("div");
+    divSong.innerText += `${songs[i].name} `;
+    divSong.addEventListener("click", () =>
+      playThisSong(songs[i], totalTracks)
+    );
+    divAllSongs.appendChild(divSong);
+  }
+
+  containerSongsList.appendChild(divAllSongs); // add ul to the container.
+};
+
+/** display albums name on Homepage */
 const albumContainer = document.getElementById('albumContainer');
 // create ul element and set its attributes.
-const ulAlbums = document.createElement('ul');
-ulAlbums.setAttribute ('style', 'padding: 0; margin: 4px;');
-ulAlbums.setAttribute('id', 'theAlbumList');
+const allAlbums = document.createElement('div');
+// allAlbums.setAttribute ('style', 'padding: 0; margin: 4px;');
+allAlbums.setAttribute('id', 'allAlbums');
 
-for ( let i = 0; i < ALBUMS.length; i++) {
+for ( let i = 0; i < albums.length; i++) {
     const li = document.createElement('li');	// create li element.
 
     li.innerHTML = `<a id="album" title="Click to open this album"
-    href="#album">${ALBUMS[i].name}</a>`;	// assigning text to li using array value.
+    href="#album">${albums[i].name}</a>`;	// assigning text to li using array value.
     li.setAttribute ('style', 'display: block;');	// remove the bullets.
-    li.addEventListener("click", () => { displayAlbumSongsNames(ALBUMS[i].id);});
+    li.addEventListener("click", () => { displayAlbumSongsNames(albums[i]);});
+    console.log("🚀 ~ file: renderer.ts:68 ~ albums[i]:", albums[i])
 
-    ulAlbums.appendChild(li);		// append li to ulAlbums.
+    allAlbums.appendChild(li);		// append li to allAlbums.
 }
-albumContainer.appendChild(ulAlbums);		// add ul to the albumContainer.
+albumContainer.appendChild(allAlbums);		// add ul to the albumContainer.
 
 
 /** function to play next song at the end of the previous one */
