f39c60f8459cd9638a59409a8425e0a2cab1e63a
Laurent Binder <laurentbinder@gmail.com>
2023-04-20 14:18:49 +0200

    add findSong() and cleaned file

--- musehit_app/src/main/preload.ts
+++ musehit_app/src/main/preload.ts
@@ -1,22 +1,33 @@
 const { contextBridge } = require('electron')
 const db = require('better-sqlite3')('./resources/jukebox.db');
 
-contextBridge.exposeInMainWorld('versions', {
+contextBridge.exposeInMainWorld("versions", {
   findAllSongs: () => {
-    const rows = db.prepare("select * from songs").all()
-    // console.log("I searched all songs")
-    return rows
+    const allSongsFromCatalog = db
+      .prepare("select * from songs")
+      .all();
+    return allSongsFromCatalog;
   },
+
   findAlbumsByArtist: () => {
-    const rows = db.prepare("SELECT * FROM  albums ORDER BY artist_id;").all()
-    // console.log("albums by artist", rows)
-    return rows
+    const rows = db
+      .prepare("SELECT * FROM  albums ORDER BY artist_id;")
+      .all();
+    return rows;
   },
 
   findAllSongsByAlbumID: (id: any) => {
-    const rows = db.prepare(`SELECT * FROM  songs WHERE album_id = ${id};`).all()
-    // console.log(`songs from album_id ${id}`, rows[0].name)
-    return rows
-  }
+    const rows = db
+      .prepare(`SELECT * FROM  songs WHERE album_id = ${id};`)
+      .all();
+    return rows;
+  },
 
-})
+  findSong: (album_id: any, song_position: 1) => {
+    const song = db
+      .prepare(`SELECT * FROM  "songs" WHERE album_id = ${album_id} AND position = ${song_position}; `)
+      .all();
+    return song[0];
+  },
+
+});
