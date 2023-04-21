9a8b6771b17623e06268f8eee80b92fba4266e64
Laurent Binder <laurentbinder@gmail.com>
2023-04-21 12:44:19 +0200

    build css with electron

--- musehit_app/esbuild.renderer.config.js
+++ musehit_app/esbuild.renderer.config.js
@@ -2,7 +2,10 @@ import * as path from 'path'
 
 module.exports = {
   platform: 'browser',
-  entryPoints: [path.resolve('src/renderer/renderer.ts')],
+  entryPoints: [
+    path.resolve('src/renderer/renderer.ts'),
+    path.resolve('src/renderer/css/index.css'),
+  ],
   bundle: true,
   target: 'chrome89', // electron version target
   loader: {
