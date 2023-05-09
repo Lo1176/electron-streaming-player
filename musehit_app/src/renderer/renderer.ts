0ef92390d3064a4ae9091b561ff0d234412410cb
Laurent Binder <laurentbinder@gmail.com>
2023-05-09 14:46:50 +0200

    refacto and conditions to avoid errors

--- musehit_app/src/renderer/renderer.ts
+++ musehit_app/src/renderer/renderer.ts
@@ -1,21 +1,23 @@
 // find all albums
 const albums = versions.findAlbumsByArtist();
-// console.log("🚀 ~ file: renderer.ts:3 ~ albums:", albums)
-
-const containerSongsList = document.getElementById("songs-list");
-const audioPlayer = <HTMLVideoElement> document.getElementById("audioPlayer");
+const songsList = document.getElementById("songs-list");
+if (!!document.getElementById("audioPlayer")) {
+  var audioPlayer = <HTMLVideoElement>document.getElementById("audioPlayer");
+}
 /************ PLAYER INFOS ***************/
-const PLAYER = console.log(
-  "🚀 ~ PLAYER_INFO ~ ",
-  "audioPlayer.paused:",
-  audioPlayer.paused,
-  "audioPlayer.currentTime:",
-  audioPlayer.currentTime,
-  "audioPlayer.ended:",
-  audioPlayer.ended,
-  "audioPlayer.readyState:",
-  audioPlayer.readyState
-);
+if (!!audioPlayer) {
+  var PLAYER = console.log(
+    "🚀 ~ PLAYER_INFO ~ ",
+    "audioPlayer.paused:",
+    audioPlayer.paused,
+    "audioPlayer.currentTime:",
+    audioPlayer.currentTime,
+    "audioPlayer.ended:",
+    audioPlayer.ended,
+    "audioPlayer.readyState:",
+    audioPlayer.readyState
+  );
+}
 /**************** END ******************/
 const isAudioPlaying = 
 !!(
@@ -36,6 +38,9 @@ const nextBtn = document.getElementById("next");
 
 /** display albums name on Homepage */
 const albumContainer = document.getElementById("albums-container");
+const albumName = document.getElementById('album-name');
+const artistName = document.getElementById('artist-name');
+const songName = document.getElementById('song-name');
 // const allAlbums = document.createElement('ul');
 // allAlbums.setAttribute('id', 'allAlbums');
 
@@ -43,44 +48,52 @@ var allSongsFromCurrentAlbum = undefined
 var totalTracks = undefined
 var currentSong = undefined
 var currentAlbum = undefined
-var artistInfo = undefined
+var currentArtist = undefined
 var defaultCover = "default-cover.png"
 
-const showAllAlbums = () => {
+// meme avec un async await j'ai deux fois mes <a></a> dans le browser. Pourquoi ?
+async function showAllAlbums() {
   for ( let i = 0; i < albums.length; i++) {
-    artistInfo = versions.findArtist(albums[i].artist_id);
+    const artistInfo = await versions.findArtist(albums[i].artist_id);
     const div = document.createElement('div');
     const cover = (albums[i].cover !== 'NULL') ? albums[i].cover : defaultCover;
+    // link to page is not working :
+    // <a id='album_${albums[i]}' href="./album.html">
     div.innerHTML = `
-     <a id="album" 
-      title="Click to open this album" 
-      href="#songs-list"><div class="album-card">
+    <a id='album_${albums[i]}' href="#songs-list">
+      <div class="album-card">
         <img class="album-cover" src="./../public/uploads/${cover}" alt="album cover">
-        <div class="album-content">
-          <h4 class="album-name">${albums[i].name}</h4>
-          <p class="album-artiste-name">${artistInfo.name}</p>
-        </div>
-    </a>`;
+      <div class="album-content">
+        <h4 class="album-name">${albums[i].name}</h4>
+        <p class="album-artiste-name">${artistInfo.name}</p>
+      </div>
+    </a>
+    `;
     div.addEventListener("click", () => { 
-      currentAlbum = albums[i].name,
-      displayAlbumSongsNames(albums[i]);
+      currentAlbum = albums[i],
+      currentArtist = artistInfo,
+      displayAlbumSongsNames();
+      // localStorage.setItem('album', currentAlbum);
+      localStorage.album = currentAlbum;
     });
-    albumContainer.appendChild(div);
+    albumContainer?.appendChild(div);
   }
 }
 showAllAlbums();
 
 /** function to display songs of an album */
-const displayAlbumSongsNames = ({id: album_id}) => {
-  allSongsFromCurrentAlbum = versions.findAllSongsByAlbumID(album_id);
+function displayAlbumSongsNames() {
+  // currentAlbum = localStorage.album
+  allSongsFromCurrentAlbum = versions.findAllSongsByAlbumID(currentAlbum.id);
   totalTracks = allSongsFromCurrentAlbum.length;
+  // console.log('currentAlbum', currentAlbum)
 
-  containerSongsList.innerHTML = "";
+  if (!!songsList) songsList.innerHTML = "";
   const divAllSongs = document.createElement("div");
-  divAllSongs.setAttribute("id", "theSongList");
+  // divAllSongs.setAttribute("id", "theSongList"); // maybe add a class but not an id
 
 
-  // list all allSongsFromCurrentAlbum from a specific album
+  // list all allSongsFromCurrentAlbum
   for (let i = 0; i < allSongsFromCurrentAlbum.length; i++) {
     const divSong = document.createElement("div");
     divSong.innerText = allSongsFromCurrentAlbum[i].name;
@@ -91,7 +104,7 @@ const displayAlbumSongsNames = ({id: album_id}) => {
     divAllSongs.appendChild(divSong);
   }
 
-  containerSongsList.appendChild(divAllSongs); // add ul to the container.
+  songsList.appendChild(divAllSongs); // add ul to the container.
   return totalTracks;
 };
 
@@ -100,7 +113,6 @@ playBtn.addEventListener("click", () => {
 });
 
 async function playPauseFctn () {
-  // console.log("🚀 ~ file: renderer.ts:97 ~ playPauseFctn ~ isAudioPlaying:", isAudioPlaying)
   if (audioPlayer.classList.contains("play")) {
     audioPlayer.classList.remove("play"),
     audioPlayer.classList.add("pause"),
@@ -114,7 +126,6 @@ async function playPauseFctn () {
   }
 }
 
-
 /** functions for players */
 // function to pay a song
 const playThisSong = () => {
@@ -122,26 +133,36 @@ const playThisSong = () => {
   audioPlayer.src = `./../public/uploads/${currentSong.path}`;
   audioPlayer.classList.add("play");
   playPauseFctn();
-
+  
   nextBtn.addEventListener("click", () => {
     nextSong();
   });
   prevBtn.addEventListener("click", () => {
     prevSong();
   });
-
+  
+  // display album informations
+  // albumName.innerHTML = // not displayed
+  songName.innerHTML = currentSong.name
+  artistName.innerHTML = currentArtist.name
   console.log("song ", currentSong.name),
-  console.log("curentAlbum: ", currentAlbum)
+    console.log(
+      "🚀 ~ file: renderer.ts:149 ~ info-content currentAlbum:",
+      currentAlbum,
+      "currentSong:",
+      currentSong,
+      "currentArtist.name:",
+      currentArtist.name
+    );
 };
+
 
 // Countdown
 audioPlayer.addEventListener("timeupdate", function() {
-    var timeleft = document.getElementById('timeleft'),
-        duration = parseInt( audioPlayer.duration ),
-        currentTime = parseInt( audioPlayer.currentTime ),
-        timeLeft = duration - currentTime,
-        s, m;
-        
+    var timeLeftElement = document.getElementById('timeleft'),
+        songDuration = parseInt(audioPlayer.duration),
+        currentTime = parseInt(audioPlayer.currentTime),
+        timeLeft = songDuration - currentTime, s, m;
         
         s = timeLeft % 60;
         m = Math.floor( timeLeft / 60 ) % 60;
@@ -149,46 +170,46 @@ audioPlayer.addEventListener("timeupdate", function() {
         s = s < 10 ? "0"+s : s;
         m = m < 10 ? "0"+m : m;
         
-        timeleft.innerHTML = m+":"+s;    
+        timeLeftElement.innerHTML = m + ":" + s;    
 }, false);
 
 // Countup
 audioPlayer.addEventListener("timeupdate", function() {
-    var timeline = document.getElementById('duration');
+    var timeLineElement = document.getElementById('duration');
     var s = parseInt(audioPlayer.currentTime % 60);
     var m = parseInt((audioPlayer.currentTime / 60) % 60);
     if (s < 10) {
-        timeline.innerHTML = m + ':0' + s;
+        timeLineElement.innerHTML = m + ':0' + s;
     }
     else {
-        timeline.innerHTML = m + ':' + s;
+        timeLineElement.innerHTML = m + ':' + s;
     }
 }, false);
 
 // Next song
-async function nextSong() {
+function nextSong() {
   let position = (currentSong.position += 1);
 
   if (position > totalTracks - 1) {
-    await audioPlayer.pause();
+    audioPlayer.pause();
   } else {
     allSongsFromCurrentAlbum = versions.findAllSongsByAlbumID(currentSong.album_id);
     currentSong = (allSongsFromCurrentAlbum.filter(
-      (song) => (song["position"] === position)))[0];
+      (song: { [x: string]: any; }) => (song["position"] === position)))[0];
     playThisSong();
   }
 };
 
 // Prev song
-async function prevSong() {
+function prevSong() {
   let position = (currentSong.position)
   if (position > 2) position -= 1;  
   if (position > totalTracks - 1) {
-    await audioPlayer.pause();
+    audioPlayer.pause();
   } else {
     const songs = versions.findAllSongsByAlbumID(currentSong.album_id);
     currentSong = (allSongsFromCurrentAlbum.filter(
-    (song) => (song["position"] === position)))[0];
+    (song: { [x: string]: any; }) => (song["position"] === position)))[0];
     playThisSong();
   }
 };
