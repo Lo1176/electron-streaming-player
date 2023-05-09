f863ee0619856b6170cb947e60176e3139128a9d
Laurent Binder <laurentbinder@gmail.com>
2023-05-09 14:45:02 +0200

    add width option and webPreferences

--- musehit_app/src/main/main.ts
+++ musehit_app/src/main/main.ts
@@ -6,20 +6,37 @@ let win: BrowserWindow | null = null
 
 async function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 820,
-    minHeight: 600,
-    minWidth: 650,
+    /** origin */ 
+    // width: 800,
+    // height: 820,
+    // minHeight: 600,
+    // minWidth: 650,
+    /** mobile */ 
+    // width: 390,
+    // height: 844,
+    // minHeight: 360,
+    // minWidth: 800,
+    /** desktop */ 
+    width: 1920,
+    height: 1080,
+    minHeight: 1366,
+    minWidth: 768,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
+      // webSecurity: false,
+      allowRunningInsecureContent: true,
+      // contextIsolation: false,
     },
     show: false,
-  })
+  });
 
   const isDev = is.development
 
-  win.loadFile('index.html')
+  win.loadFile('./index.html')
+  // win.loadFile('album.html')
+  // win.loadUrl(`/album.html`);
+
 
   win.on('closed', () => {
     win = null
@@ -52,3 +69,11 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+// testing to get data ...
+// app.on('window-all-closed', (event, data) => {
+//   win.loadFile("profil.html");
+//   win.webContents.on("did-finish-load", () => {
+//     win.webContents.send("userData", data);
+//   });
+// });
