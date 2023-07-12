0c32c3ad6548a67e91fa1637565676e6dce33eb1
Laurent Binder <laurentbinder@gmail.com>
2023-07-12 17:52:33 +0200

    add searchBarFunction function

--- musehit_app/src/renderer/utils/searchBarFunction.ts
+++ musehit_app/src/renderer/utils/searchBarFunction.ts
@@ -1,5 +1,22 @@
+/********* Search Bar Function  ************/
+
 const searchBarFunction = () => {
-    
+    const searchInput = document.querySelector(
+    "[data-search]"
+    ) as HTMLInputElement | null;
+    const dataAlbumContainer = document.querySelector(
+    "[data-album-cards-container]"
+    ) as HTMLInputElement | null;
+
+    searchInput.addEventListener("input", (e) => {
+        const value = (e.target as HTMLInputElement | null)?.value.toLowerCase();
+        const targetCard = document.getElementsByClassName("album-card");
+        let searchDataFiles =[];
+        if (value.length >= 2) {
+            searchDataFiles = versions.searchData(value);
+        }
+        return searchDataFiles
+    });
 }
 
 export default searchBarFunction;
