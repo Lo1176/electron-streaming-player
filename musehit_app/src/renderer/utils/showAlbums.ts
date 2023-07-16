2fbd290af5c05fd306516c9aee061aee661f3748
Laurent Binder <laurentbinder@gmail.com>
2023-07-17 00:07:58 +0200

    can t display songs from album anymore ...

--- musehit_app/src/renderer/utils/showAlbums.ts
+++ musehit_app/src/renderer/utils/showAlbums.ts
@@ -2,40 +2,44 @@
  * function to display all albums
  */
 
+import { Album } from "../renderer";
 import { displayAlbumSongsNames } from "./displayAlbumSongsNames";
+
 
   let albumContainer = document.getElementById("albums-container");
   let defaultCover = "default-cover.png";
   let currentAlbum = undefined;
   let currentArtist = undefined;
-  
-  
-  async function showAllAlbums(albums) {
-    albumContainer.innerHTML = ""
-    for (let i = 0; i < albums.length; i++) {
-    const artistInfo = await versions.findArtistById(albums[i].artist_id);
-    const albumCard = document.createElement("div");
-    const cover = albums[i].cover !== "NULL" ? albums[i].cover : defaultCover;
-    // <a id='album_${albums[i]}' href="./../src/renderer/album.html">
-    // <a id='album_${albums[i]}' href="#songs-list">
-    albumCard.innerHTML = `
-    <a id='album_${albums[i]}' href="./../src/renderer/album.html">
-    <div class="album-card">
-        <img class="album-cover" src="./../public/uploads/${cover}" alt="album cover">
-      <div class="album-content">
-        <h4 class="album-name">${albums[i].name}</h4>
-        <p class="album-artiste-name">${artistInfo.name}</p>
-      </div>
-    </a>
-    `;
-    albumCard.addEventListener("click", () => {
-      (currentAlbum = albums[i]),
-        (currentArtist = artistInfo),
-        displayAlbumSongsNames();
-      localStorage.album = currentAlbum;
-    });
-    albumContainer?.appendChild(albumCard);
-  }
+
+  async function showAllAlbums(albums: Album) {
+    // if (albumContainer) {
+      // albumContainer.innerHTML = ""
+      for (let i = 0; i < albums.length; i++) {
+      const artistInfo = await versions.findArtistById(albums[i].artist_id);
+      const albumCard = document.createElement("div");
+      const cover = albums[i].cover !== "NULL" ? albums[i].cover : defaultCover;
+      // <a id='album_${albums[i]}' href="#songs-list">
+      albumCard.innerHTML = `
+      <div class="album-card">
+      <a id='album_${albums[i]}' href="../src/renderer/album.html">
+          <img class="album-cover" src="../public/uploads/${cover}" alt="album cover">
+        <div class="album-content">
+          <h4 class="album-name">${albums[i].name}</h4>
+          <p class="album-artiste-name">${artistInfo.name}</p>
+        </div>
+      </a>
+      `;
+      albumCard.addEventListener("click", () => {
+        currentAlbum = albums[i]
+        currentArtist = artistInfo
+        displayAlbumSongsNames(currentAlbum);
+        });
+        albumContainer?.appendChild(albumCard);
+      }
+    // } else {
+      // console.log('else ...')
+      // return localStorage.album
+  // }
 }
 
 export { showAllAlbums, currentArtist, currentAlbum };
