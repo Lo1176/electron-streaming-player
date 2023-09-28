71129df5c39858982ce82e456d45c7d6168177d3
Laurent Binder <laurentbinder@gmail.com>
2023-09-28 16:47:55 +0200

    get ride of comments

--- musehit_app/src/main/main.ts
+++ musehit_app/src/main/main.ts
@@ -1,23 +1,23 @@
 import { app, BrowserWindow, ipcMain } from "electron";
-import { is } from 'electron-util'
+import { is } from "electron-util";
 import fs from "fs";
-import path from 'path'
+import path from "path";
 
-let win: BrowserWindow | null = null
+let win: BrowserWindow | null = null;
 
 async function createWindow() {
   win = new BrowserWindow({
-    /** origin */ 
+    /** origin */
     // width: 800,
     // height: 820,
     // minHeight: 600,
     // minWidth: 650,
-    /** mobile */ 
+    /** mobile */
     // width: 390,
     // height: 844,
     // minHeight: 360,
     // minWidth: 800,
-    /** desktop */ 
+    /** desktop */
     width: 1920,
     height: 1080,
     // width: 1200,
@@ -34,65 +34,52 @@ async function createWindow() {
     show: false,
   });
 
-  const isDev = is.development
+  const isDev = is.development;
 
-  win.loadFile('./index.html')
+  win.loadFile("./index.html");
 
-  win.on('closed', () => {
-    win = null
-  })
+  win.on("closed", () => {
+    win = null;
+  });
 
-  win.webContents.on('devtools-opened', () => {
-    win!.focus()
-  })
+  win.webContents.on("devtools-opened", () => {
+    win!.focus();
+  });
 
-  win.on('ready-to-show', () => {
-    win!.show()
-    win!.focus()
+  win.on("ready-to-show", () => {
+    win!.show();
+    win!.focus();
 
     if (isDev) {
-      win!.webContents.openDevTools({ mode: 'bottom' })
+      win!.webContents.openDevTools({ mode: "bottom" });
     }
-  })
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!is.macos) {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (win === null && app.isReady()) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-ipcMain.on('ondragstart', (event, filePath) => {
+ipcMain.on("ondragstart", (event, filePath) => {
   readFile(filePath);
 
   function readFile(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
+    fs.readFile(filePath, "utf-8", (err, data) => {
       if (err) {
-        alert('an error occurred in reading the file: ' + err.message);
+        alert("an error occurred in reading the file: " + err.message);
         return;
       }
-      event.sender.send('fileData', data)
-    })
-
+      event.sender.send("fileData", data);
+    });
   }
-})
-
-// testing to get data ...
-// app.on('window-all-closed', (event, data) => {
-//   win.loadFile("profil.html");
-//   win.webContents.on("did-finish-load", () => {
-//     win.webContents.send("userData", data);
-//   });
-// });
-
-/* [ROMAIN]
-  pense à nettoyer les commentaires dont tu ne feras plus usage
-*/
+});
